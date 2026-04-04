b02ce9403c79215c431616c970a1d7ef5d15f1e1
sebasGZA <sebassga34@gmail.com>
2026-04-04 16:34:56 -0500

    fix: add isActive value

--- src/auth/repositories/user.repository.ts
+++ src/auth/repositories/user.repository.ts
@@ -19,6 +19,7 @@ export class UserRepository extends Repository<User> {
                 id: true,
                 email: true,
                 password: true,
+                isActive: true,
             }
         })
     }
