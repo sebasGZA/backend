2cb2cbbfe3bffe1ff3fcc306b7030d6b57538c2d
sebasGZA <sebassga34@gmail.com>
2026-04-06 14:50:41 -0500

    fix: change cookies

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -61,7 +61,8 @@ export class AuthService {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-            maxAge: 1000 * 60 * 60 * 24
+            maxAge: 1000 * 60 * 60 * 24,
+            path: '/',
         });
         return token;
     }
