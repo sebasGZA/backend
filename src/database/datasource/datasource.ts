c5d761b6122ac463867a9f57eb72829c74a7741f
sebasGZA <sebassga34@gmail.com>
2026-04-06 13:11:05 -0500

    fix: change datasource

--- src/database/datasource/datasource.ts
+++ src/database/datasource/datasource.ts
@@ -6,11 +6,7 @@ import { User } from '../../user/entities/user.entity';
 
 export const dataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    url: process.env.DB_URL,
     entities: [User, Post],
     migrations: ['src/migrations/*{.ts,.js}'],
     synchronize: false,
